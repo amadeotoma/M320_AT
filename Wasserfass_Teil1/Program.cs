57a78c2149afdd7f0cdbe5d814dd62001d8ceb5b
amadeotoma <142779682+amadeotoma@users.noreply.github.com>
2023-10-29 22:02:41 +0100

    Add files via upload

---
+++ Wasserfass_Teil1/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Wasserfass_Teil1
+{
+    internal class Program
+    {
+        public class Wasserfass
+        {
+            private double MinimalPegel;
+            private double MaximalPegel;
+            private double Füllstand;
+            private double Kapazität;
+
+            public Wasserfass(double minimalPegel, double maximalPegel, double kapazität)
+            {
+                MinimalPegel = minimalPegel;
+                MaximalPegel = maximalPegel;
+                Kapazität = kapazität;
+                Füllstand = minimalPegel;
+            }
+
+            public void befüllen(double menge)
+            {
+                if (Füllstand + menge <= Kapazität && Füllstand + menge <= MaximalPegel)
+                {
+                    Füllstand += menge;
+                }
+            }
+
+            public void entnehmen(double menge)
+            {
+                if (Füllstand - menge >= MinimalPegel)
+                {
+                    Füllstand -= menge;
+                }
+            }
+
+            public void entleeren()
+            {
+                Füllstand = MinimalPegel;
+            }
+        }
+    }
+    }
