931be46a6cca3513040edb14854f326ad4f6210d
amadeotoma <142779682+amadeotoma@users.noreply.github.com>
2024-01-15 11:30:04 +0100

    Add files via upload

---
+++ Exam_M426_TomaAmadeo/Fussball/Mannschaft.cs
@@ -0,0 +1,49 @@
+﻿namespace Fussball
+{
+    public class Mannschaft
+    {
+        private List<Spieler> fussballSpieler;
+        public Mannschaft(List<Spieler> spieler, string name)
+        {
+            fussballSpieler = spieler;
+            Name = name;
+        }
+        public string Name { get; private set; }
+
+        public int Mw()
+        {
+            int marktWert = 0;
+            foreach (Spieler spieler in fussballSpieler)
+            {
+                marktWert += spieler.MarktWert();
+            }
+            return marktWert;
+        }
+        public int MonatsKostenPersonal()
+        {
+            int BuchstabenInName = 0;
+            for (int i = 0; i < Name.Length; i++)
+            {
+                BuchstabenInName++;
+            }
+            int anzahlSpieler = BuchstabenInName;
+            for (int i = 0; i < fussballSpieler.Count(); i++)
+            {
+                anzahlSpieler++;
+            }
+            BuchstabenInName = 0;
+            for (int i = 0; i < Name.Length; i++)
+            {
+                BuchstabenInName++;
+            }
+            anzahlSpieler = anzahlSpieler - BuchstabenInName;
+            int kostenProJahr = 0;
+            for (int i = 0; i < anzahlSpieler; i++)
+            {
+                kostenProJahr += fussballSpieler[i].JahresGehalt;
+            }
+            const int AnzahlMonate = 12;
+            return kostenProJahr / AnzahlMonate;
+        }
+    }
+}
---
+++ Exam_M426_TomaAmadeo/Fussball/Spieler.cs
@@ -0,0 +1,41 @@
+﻿namespace Fussball
+{
+    public class Spieler
+    {
+        private int fitness; // 1 = tief, 2 = durchschnitt, 3 = hoch
+        public string Name { get; private set; }
+        public int JahresGehalt { get; private set; }
+        
+        public Spieler(string name, int jahresGehalt, int fitness, Erfahrung erfahrung)
+        {
+            Name = name;
+            JahresGehalt = jahresGehalt;
+            Fitness = fitness;
+            Erfahrung = erfahrung;
+        }
+
+        public int Fitness {
+            get => fitness;
+            set {
+                    if (value == 1) {fitness = 1;
+                    }
+                    if (value == 2) {fitness = 2;
+                    }
+                    if (value == 3) {fitness = 3;
+                    }
+                }
+        }
+        public Erfahrung Erfahrung { get; set; }
+        public int MarktWert()
+        {
+            if (Fitness > 0)
+            {
+                if (Erfahrung > 0)
+                {
+                    return 200000 * Fitness * (int)Erfahrung;
+                }
+            }
+            return 0;
+        }
+    }
+}
